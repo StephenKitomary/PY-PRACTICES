db6c0850af3d0abc56c493377d6da07f2b9dec46
Stephen Kitomary <kitomarystephen@gmail.com>
2024-09-28 13:33:27 -0400

    Initial commit

---
+++ Sep2824.py
@@ -0,0 +1,18 @@
+#############################################################################
+#           VARIABLES
+#############################################################################
+
+"""
+memory references
+memory management
+references counting 
+garbage collection
+dynamic and static typing 
+mutability and immutability
+shared references
+Variable equality
+"""
+
+ # my_var = 10 , technically my_var is not equal to ten, itself references a  memory address to which 10 is held or stored, therefore variables in python are always references to objects in memory
+
+# in python we can find the memory address of the variable by using the id() function
