94d6105427e1f2eea5631cce2f83ced719b63b31
Stephen Kitomary <kitomarystephen@gmail.com>
2024-09-28 22:24:14 -0400

    100-1 p2

--- 100-1.py
+++ 100-1.py
@@ -1,5 +1,24 @@
+"""
 print("Hello "+input("What is your name?"))
 
 name = str(input("what is your name?"))
 
 print(f"hello {name}! How are you doing today?")
+
+"""
+"""
+print("Welcome to the Brand Name Generator")
+city = str(input("Whats the name of the city that you grew up in?\n"))
+pet = str(input("What's your pet's name?\n"))
+print(f"Your brand name could be {city} {pet}")
+"""
+
+print("Welcome to the tip calculator.")
+total = float(input("What was the total bill? $"))
+tip = int(input("What percentage tip would you like to give? 10, 12, or 15? "))
+people = int(input("How many people to split the bill? "))
+
+total_tip = total + ((tip/100)*total)
+each = round(total_tip/people, 2)
+
+print(f"Each person should pay: ${each}")
