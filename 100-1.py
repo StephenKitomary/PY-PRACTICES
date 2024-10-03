318a98eda1f3d01030ff86233893386a06e8e6ee
Stephen Kitomary <kitomarystephen@gmail.com>
2024-10-02 23:11:44 -0400

    random choices

--- 100-1.py
+++ 100-1.py
@@ -12,7 +12,7 @@ city = str(input("Whats the name of the city that you grew up in?\n"))
 pet = str(input("What's your pet's name?\n"))
 print(f"Your brand name could be {city} {pet}")
 """
-
+"""
 print("Welcome to the tip calculator.")
 total = float(input("What was the total bill? $"))
 tip = int(input("What percentage tip would you like to give? 10, 12, or 15? "))
@@ -22,3 +22,10 @@ total_tip = total + ((tip/100)*total)
 each = round(total_tip/people, 2)
 
 print(f"Each person should pay: ${each}")
+"""
+
+names = input("Gemme the names of the people separated by a comma")
+
+names_list = names.split(",")
+
+print(names_list[0])
