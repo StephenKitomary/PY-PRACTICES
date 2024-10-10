aa7086b2cbc253fdc27cfac9010ce79a72ba3647
Stephen Kitomary <kitomarystephen@gmail.com>
2024-10-10 06:38:24 -0400

    qn2

--- Homework 6.py
+++ Homework 6.py
@@ -1,4 +1,4 @@
-def largest_odd(w, x, y, z):
+"""def largest_odd(w, x, y, z):
     largest = None
     for num in [w, x, y, z]:
         if num % 2 != 0:
@@ -9,3 +9,19 @@ def largest_odd(w, x, y, z):
         return -1
     else:
         return largest
+    """
+def largest_odd(w=1, x=2, y=3, z=4):
+    largest = None
+    
+    for num in [w, x, y, z]:
+        if num % 2 != 0:
+            if largest is None or num > largest:
+                largest = num
+    
+    if largest is None:
+        return -1
+    else:
+        return largest
+
+
+print(largest_odd()) 
