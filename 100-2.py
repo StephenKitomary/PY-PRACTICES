1df591cf9f335655eafb08d925d9bb76deed133a
Stephen Kitomary <kitomarystephen@gmail.com>
2024-10-12 13:02:57 -0400

    fizzbuzz

--- 100-2.py
+++ 100-2.py
@@ -1,4 +1,4 @@
-print('Welcome to Tressure Island.\nYour mission is to find the tressure.')
+"""print('Welcome to Tressure Island.\nYour mission is to find the tressure.')
 
 choice = input("left or right?")
 if choice.lower() == "right":
@@ -21,4 +21,26 @@ else:
         else:
             
             print("Invalid Choice")
+""" 
 
+"""students = input("Input a list of student heights ").split()
+print(students)
+answer = 0 
+for n in range(0, len(students)):
+    students[n] = int(students[n])
+    answer = answer + students[n]
+
+print(students)
+print(sum(students))
+print(answer)
+"""
+
+for i in range (1,101):
+    if i%3==0 and i%5 == 0:
+        print("Fizzbuzz")
+    elif i%3 == 0:
+        print("Fizz")
+    elif i%5 == 0:
+        print("Buzz")
+    else:
+        print(i)
