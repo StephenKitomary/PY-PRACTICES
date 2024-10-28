c1861f28cf549bef2d9df660ff8237d7b2d8de02
Stephen Kitomary <kitomarystephen@gmail.com>
2024-10-28 10:49:33 -0400

    some updates

--- homework3.py
+++ homework3.py
@@ -19,3 +19,25 @@ for i in word:
 
 print(hangman)
 """
+"""
+# Function to generate Fibonacci sequence
+def generate_fibonacci(n):
+    fibonacci_sequence = []
+    a, b = 0, 1
+    for _ in range(n):
+        fibonacci_sequence.append(a)
+        a, b = b, a + b
+    return fibonacci_sequence
+
+# Ask the user for how many Fibonacci numbers they want to generate
+num = int(input("How many Fibonacci numbers would you like to generate? "))
+
+# Generate and print the Fibonacci sequence
+if num <= 0:
+    print("Please enter a positive integer.")
+else:
+    fibonacci_sequence = generate_fibonacci(num)
+    print("The first", num, "Fibonacci numbers are:")
+    for number in fibonacci_sequence:
+        print(number)
+"""
---
+++ practice.py
@@ -0,0 +1,13 @@
+"""word = "Banana"
+
+newword=""
+for i in word:
+    newword=newword+i
+
+print(newword)
+
+"""
+for k in range(2,18):
+    if k%2 == 0
+        print(k)
+
