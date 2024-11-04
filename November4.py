551e30f00c70f405710af05a9ccf6ca660e1a0a3
Stephen Kitomary <kitomarystephen@gmail.com>
2024-11-04 06:50:10 -0500

    Exercise 2

--- November4.py
+++ November4.py
@@ -29,3 +29,18 @@ weights_lb = list(map(kg_to_lbs,weights_kg))
 print(weights_lb)"""
 
 
+recent_visitors = [101, 102, 103, 101, 104, 105, 102, 106]
+
+unique_recent_visitors = set(recent_visitors)
+print("These are the unique visitors: ",unique_recent_visitors)
+
+recent_purchasers = {102, 103, 107, 108}
+print("These are the customers who visited us recently and made purchases: ", recent_purchasers.intersection(unique_recent_visitors))
+
+vip_customers = {105, 106, 109, 110}
+
+resent_and_vip_customers = unique_recent_visitors.union(vip_customers)
+print("These are the resent and VIP visitors", resent_and_vip_customers)
+
+VIP_but_not_recent = vip_customers.difference(unique_recent_visitors)
+print("These are VIP customers ut did not visit our website recently", VIP_but_not_recent)
