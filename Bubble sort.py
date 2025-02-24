a118b634777cee6eb3b225d89c46e62d41cb2897
Stephen Kitomary <kitomarystephen@gmail.com>
2025-02-24 15:21:46 -0500

    Bubble sort completed

--- Bubble sort.py
+++ Bubble sort.py
@@ -1,3 +1,12 @@
+"""Begin by loading Numbers.txt into a list.This file contains 100 integers between 0 and 1000D
+efine the function load_numbers(filename), where filename is a formal parameter that is a string, containing the name of the file you want to load.
+You should return a list that contains the (unsorted) numbers in filename as a list."""
+
+def load_numbers(filename):
+    with open(filename) as file:
+        numbers = file.readlines()
+    return [int(number) for number in numbers]
+
 def bubble_sort(arr):
     for i in range(len(arr)-1,0,-1):
         for j in range(i):
@@ -5,4 +14,4 @@ def bubble_sort(arr):
                 arr[j], arr[j+1] = arr[j+1], arr[j]
     return arr
 
-print(bubble_sort([3,2,1,5,4,6,7,8,9,0]))
+print(bubble_sort(load_numbers("Numbers.txt")))
