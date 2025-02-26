a60553519a2efa632dd39b411831e1fb2c8816df
Stephen Kitomary <kitomarystephen@gmail.com>
2025-02-26 14:54:52 -0500

    Quick Sort Pivot and Swap

---
+++ Quick Sort.py
@@ -0,0 +1,13 @@
+def pivot(my_list, pivot_index, end_index):
+    swap_index = pivot_index
+
+    for i in range(pivot_index+1, end_index+1):
+        if my_list[i] < my_list[pivot_index]:
+            swap_index += 1
+            swap(my_list, swap_index, i)
+    swap(my_list, pivot_index, swap_index)
+    return swap_index
+
+def swap(my_list,index_1, index_2):
+   my_list[index_1], my_list[index_2] = my_list[index_2], my_list[index_1]
+
