b33723511e8b59302c0546d3b3a16e345076f74f
Stephen Kitomary <kitomarystephen@gmail.com>
2025-02-26 18:41:19 -0500

    sort

---
+++ Quick Sort.py
@@ -0,0 +1,8 @@
+def sort(My_List, Start, End):
+    pass
+
+def pivot(My_List,swap, end):
+    pass
+
+def swap(My_List, index1, index2):
+    My_List[index1], My_List[index2] = My_List[index2], My_List[index1]
