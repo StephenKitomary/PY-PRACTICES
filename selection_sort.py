76ffe099add50a93f924eea83816acb686f5e16e
Stephen Kitomary <kitomarystephen@gmail.com>
2025-02-27 17:57:43 -0500

    selection_sort working

--- selection_sort.py
+++ selection_sort.py
@@ -4,4 +4,10 @@ def selection_sort(My_List):
         for j in range(i+1, len(My_List)):
             if My_List[j]<My_List[min_index]:
                 min_index = j
-        My_List[i],My_List[min_index] = My_List[min_index], My_List[i]
+        if i != min_index:
+            My_List[i],My_List[min_index] = My_List[min_index], My_List[i]
+    return My_List
+
+My_List = [64, 25, 12, 22, 11]
+print(selection_sort(My_List))
+
