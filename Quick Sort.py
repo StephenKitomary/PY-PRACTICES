e1b13804fb22f29db73b3b68963485b1e35d61fd
Stephen Kitomary <kitomarystephen@gmail.com>
2025-02-26 19:44:41 -0500

    pivot done

--- Quick Sort.py
+++ Quick Sort.py
@@ -1,8 +1,17 @@
 def sort(My_List, Start, End):
-    pass
+    
 
-def pivot(My_List,swap, end):
-    pass
+def pivot(My_List,pivot_index, end_index):
+    swap_index = pivot_index
+    for i in range(pivot_index+1, end_index+1):
+        if My_List[i]<My_List[pivot_index]:
+            swap_index += 1
+            swap(My_List,swap_index,i)
+    swap(My_List, pivot_index, swap_index)
+    return swap_index
+
+
 
 def swap(My_List, index1, index2):
     My_List[index1], My_List[index2] = My_List[index2], My_List[index1]
+3
