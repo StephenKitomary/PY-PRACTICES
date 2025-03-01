3a38982adf0980b92799b76647741ea7b987bbae
Stephen Kitomary <kitomarystephen@gmail.com>
2025-03-01 17:59:19 -0500

    another update

--- Quick Sort.py
+++ Quick Sort.py
@@ -2,10 +2,9 @@ def quick_sort(My_List, left, right):
     if left < right:
         pivot_index = pivot(My_List,left, right)
         quick_sort(My_List, left, pivot_index-1)
-        quick_sort(My_List, pivot_index+1, right)
+        quick_sort(My_List, pivot_index+1, right) 
     return My_List
 
-    
 
 def pivot(My_List,pivot_index, end_index):
     swap_index = pivot_index
@@ -17,9 +16,8 @@ def pivot(My_List,pivot_index, end_index):
     return swap_index
 
 
-
 def swap(My_List, index1, index2):
     My_List[index1], My_List[index2] = My_List[index2], My_List[index1]
 
-List = [4,6,2,1,7,3,8,5]
+List = [4,6,2,1,7,7,3,8,5]
 print(quick_sort(List, 0, len(List)-1))
