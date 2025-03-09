c58d52996438a5792cb3d75a6f4501fab4d48b33
Stephen Kitomary <kitomarystephen@gmail.com>
2025-03-08 19:47:09 -0500

    new

--- Quick Sort.py
+++ Quick Sort.py
@@ -11,3 +11,13 @@ def pivot(my_list, pivot_index, end_index):
 def swap(my_list,index_1, index_2):
    my_list[index_1], my_list[index_2] = my_list[index_2], my_list[index_1]
 
+def quick_sort(my_list, left, right):
+    if left < right:
+        pivot_index = pivot(my_list, left,right)
+        quick_sort(my_list, left, pivot_index-1)
+        quick_sort(my_list, pivot_index+1,right)
+    return my_list
+my_l = [8,9,1,7,3,5,2,6,4]
+print(my_l)
+quick_sort(my_l,0,len(my_l)-1)
+print(my_l)
