7d43ace106820c5d57156302c0da1462f460cdd2
Stephen Kitomary <kitomarystephen@gmail.com>
2025-03-10 09:07:42 -0400

    new work

---
+++ Mar_10.py
@@ -0,0 +1,22 @@
+class Int_set(object):
+    """ An Int_set is a set of integers"""
+
+    def __int__(self):
+        """Create an empty set of integers"""
+        self._vals = []
+    def insert(self,e):
+        """Assumes e is an integer and inserts e into self"""
+        if e not in self._vals:
+            self._vals.append(e)
+    def member(self,e):
+        """Assumes e is an integer Returns True is e is in self, False otherwise"""
+        return e in self._vals
+
+    def remove(self, e):
+        """Assumes e is an integer and removes e from self, Raises Value error if e is not in self"""
+        try:
+            self._vals.remove(e)
+        except:
+            raise Valueeror(str(e)+ "not found")
+
+    
